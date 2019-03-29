a9028f21730a3509c0c0e3c280508b3225afcf98
ta7sudan <ta7sudan@gmail.com>
2019-03-30 00:14:39 +0800

    use eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,16 +6,29 @@ module.exports = {
 	},
 	// 不要用eslint-config-prettier, 那是个傻逼东西, 只用eslint-plugin-prettier就好
 	// 'extends': ['eslint:recommended', 'plugin:prettier/recommended'],
-	extends: 'eslint:recommended',
-	plugins: ['prettier'],
+	extends: 'plugin:@typescript-eslint/recommended',
+	plugins: ['@typescript-eslint'],
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		ecmaVersion: 2018,
-		sourceType: 'module'
+		project: './tsconfig.json',
+		extraFileExtensions: ['.ts', '.js']
 	},
 	rules: {
 		'no-console': 'off',
 		'no-debugger': 'off',
 		'no-extra-boolean-cast': 'off',
+		'@typescript-eslint/array-type': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-object-literal-type-assertion': 'off',
+		'@typescript-eslint/no-use-before-define': 'off',
+		'@typescript-eslint/indent': [
+			'error',
+			'tab',
+			{
+				SwitchCase: 1,
+				VariableDeclarator: 1
+			}
+		],
 		indent: [
 			'error',
 			'tab',
